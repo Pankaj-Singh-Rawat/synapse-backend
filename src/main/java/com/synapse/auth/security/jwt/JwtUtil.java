775d58be807cfb0e46dbe24b49e67c8b192c7972
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-01-20 22:38:37 +0530

    added jwtUtil for filtering purposes

---
+++ src/main/java/com/synapse/auth/security/jwt/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.synapse.auth.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+    private final SecretKey secretKey;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
