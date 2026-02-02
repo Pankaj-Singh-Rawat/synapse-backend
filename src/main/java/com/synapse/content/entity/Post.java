cf280f5d21791d31258f567fd9371b24f50ca0d7
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-02-02 08:59:59 +0530

    Post entitiy created

---
+++ src/main/java/com/synapse/content/entity/Post.java
@@ -0,0 +1,55 @@
+package com.synapse.content.entity;
+
+import com.synapse.user.entity.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(optional = false)
+    private User creator;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    private LocalDateTime localDateTime;
+
+    public Post() {
+    }
+
+    public Post(User creator, String type, String content) {
+        this.creator = creator;
+        this.type = type;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+}
